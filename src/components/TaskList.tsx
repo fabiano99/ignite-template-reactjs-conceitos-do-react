b70eb73e698c2fdf0f5e26b8bdb7a9d1c0967952
Fabiano Moreira <fabiano.moreira-ext@viavarejo.com.br>
2021-11-02 03:33:42 -0300

    feat(task): implemented functions to add, remove e mark as done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,30 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle.trim()) return;
+    const taskId = Math.random() * 100;
+    const newTask: Task = {
+      id: taskId,
+      title: newTaskTitle,
+      isComplete: false
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if (task.id === id) return {...task, isComplete: !task.isComplete};
+      return task;
+    });
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => {
+      return task.id !== id;
+    });
+    setTasks(newTasks);
   }
 
   return (
